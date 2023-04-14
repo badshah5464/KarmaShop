151d47198535d7fe0f684deb6e4cbdacbab353e3
Dev Dave <115370760+badshah5464@users.noreply.github.com>
2023-04-15 03:25:04 +0530

    header style complete but not responsive.

---
+++ Header.js
@@ -0,0 +1,52 @@
+//* Navigation bar scroll response. 
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop < 50) {
+        document.getElementById("headerScroll").className = "header-top";
+    } else {
+        document.getElementById("headerScroll").className = "header-scroll";
+    }
+}
+
+//* Search penal 
+//! (Under Working : cancel btn is not working!) 
+const searchIcon = document.getElementById("searchIcon");
+const searchPenal = document.getElementById("searchPenal");
+// const cancelBtn = document.getElementById("cancelBtn")
+
+searchIcon.addEventListener("click", () => {
+    if (searchPenal.className === "search-penal") {
+        searchPenal.className += " search-penal-response";
+    } else {
+        searchPenal.className = "search-penal";
+    }
+});
+
+//* li hover drop down
+function shop() {
+    document.querySelector("#shop").classList.toggle("show")
+}
+
+function blog() {
+    document.querySelector("#blog").classList.toggle("show")
+}
+
+function pages() {
+    document.querySelector("#pages").classList.toggle("show")
+}
+
+window.onclick = (event) => {
+    if (!event.target.matches(".drop")) {
+        let down = document.getElementsByClassName("down")
+        let i;
+        for (i = 0; i < down.length; i++) {
+            let dropdown = down[i]
+            if (dropdown.classList.contains('show')) {
+                dropdown.classList.remove('show')
+            }
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,36 @@
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function plushSlide(n) {
+    showSlide(slideIndex += n)
+};
+
+function plushSlide(n) {
+    showSlide(slideIndex = n)
+};
+
+function showSlide(n) {
+    let i;
+    let slide = document.getElementsByClassName('slideshow');
+    if (n > slide.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slide.length
+    }
+    for (i = 0; i < slide.length; i++) {
+        slide[i].style.display = 'none';
+    }
+    slide[slideIndex - 1].style.display = "block";
+}
+
+
+let links = document.getElementsByClassName('animated-link');
+
+for (i of links) {
+    (function (i) {
+        i.addEventListener('click', function (e) {
+            e.stopPropagation();
+        });
+    })(i);
+}
